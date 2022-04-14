67845d744819ef659400750debb51a9a3f771ca8
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2022-04-15 00:18:46 +0300

    Basic module outline

--- extension/extension.js
+++ extension/extension.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const vscode = require('vscode')
 const HexaLinter = require('./hexa-linter')
 
@@ -19,4 +21,67 @@ exports.activate = function (context) {
     workspace.onDidDeleteFiles((event) => { linter.onDidDeleteFiles(event) })
     workspace.onDidCloseTextDocument((document) => { linter.onDidCloseTextDocument(document) })
     workspace.onDidRenameFiles((event) => { linter.onDidRenameFiles(event) })
+
+    const sel = { scheme: 'file', language: 'hexa' }
+
+    class HexaConfigDocumentSymbolProvider {
+        provideDocumentSymbols(
+            document, // vscode.TextDocument
+            token // vscode.CancellationToken
+        )
+        // Promise<vscode.DocumentSymbol[]>
+        {
+            return new Promise((resolve, reject) => {
+                let symbols = []
+                var re = new RegExp("([a-z]+) ([A-z]+)")
+                let children = null
+                for (var i = 0; i < document.lineCount; i++) {
+                    var line = document.lineAt(i)
+                    var text = line.text.trim()
+                    if (
+                        ["class ", "enum ", "var ", "let ", "fun "].some(_ => text.startsWith(_))
+                    ) {
+                        let isType = text.startsWith("class ") || text.startsWith("enum ")
+                        if (isType) {
+                            children = []
+                        }
+
+                        let symbol = new vscode.DocumentSymbol(
+                            re.exec(text)[2],
+                            re.exec(text)[1],
+                            {
+                                'class': vscode.SymbolKind.Class,
+                                'enum': vscode.SymbolKind.Enum,
+                                'var': vscode.SymbolKind.Variable,
+                                'let': vscode.SymbolKind.Constant,
+                                'fun': vscode.SymbolKind.Function,
+                            }[re.exec(text)[1]] ?? vscode.SymbolKind.Function,
+                            line.range,
+                            line.range
+                        )
+
+                        symbol.children = isType ? children : []
+
+                        if (children && !isType) {
+                            children.push(symbol)
+                        } else {
+                            symbols.push(symbol)
+                        }
+                    }
+                }
+                resolve(symbols)
+            })
+        }
+    }
+
+    const providerSymbol = vscode.languages.registerDocumentSymbolProvider(
+        sel,
+        new HexaConfigDocumentSymbolProvider()
+    )
+
+    context.subscriptions.push(
+        provider2,
+        provider1,
+        providerSymbol
+    )
 }
